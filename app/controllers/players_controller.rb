135c3c11f259d107b7591db5b695016cc9eb0d1f
zac <zmb1391@gmail.com>
2014-02-16 16:15:11 -0500

    added team password verification to new player form

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -70,6 +70,6 @@ class PlayersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def player_params
-      params.require(:player).permit(:first_name, :last_name, :phone, :email, :team_id)
+      params.require(:player).permit(:first_name, :last_name, :phone, :email, :team_id, :password)
     end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,20 @@
 class Player < ActiveRecord::Base
+	attr_accessor :password
 	belongs_to :team
 
 	validates :first_name, :last_name, :phone, :email, presence: true
 	validates :phone, length: {is: 10}
+	validate :password_matches_team
+
+	def password_matches_team
+		team = Team.find(team_id)
+		if team.nil?
+			errors.add(:password, "invalid team find")
+		end
+		if !team.has_password?(password)
+			errors.add(:password, "invalid team password")
+		end
+	end
+
 end
+
