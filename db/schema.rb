f3b6b992c1d392d23d8bc0f29eba46bcd6a4a5a2
zac <zmb1391@gmail.com>
2014-02-18 12:39:27 -0500

    added end_time to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217005857) do
+ActiveRecord::Schema.define(version: 20140218170445) do
 
   create_table "coaches", force: true do |t|
     t.string   "first_name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140217005857) do
     t.datetime "updated_at"
     t.date     "the_date"
     t.time     "the_time"
+    t.time     "end_time"
   end
 
   create_table "eventtypes", force: true do |t|
