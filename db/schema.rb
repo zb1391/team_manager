74ac8dc0137697d20b625e6c98f4e26abc45cc0a
zac <zmb1391@gmail.com>
2014-02-20 02:10:33 -0500

    removed parents migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220043709) do
+ActiveRecord::Schema.define(version: 20140220004605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 20140220043709) do
     t.integer  "team_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "parents",    default: [], array: true
   end
 
   create_table "teams", force: true do |t|
