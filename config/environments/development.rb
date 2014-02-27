a8b8bb3afdcdff26d35dcbe361e8f9370bda9528
zac <zmb1391@gmail.com>
2014-02-26 20:01:21 -0500

    bug fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,16 +27,22 @@ TeamManager::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "heroku.com",
-    :user_name            => "zmb1391@gmail.com",
-    :password             => ENV["MAIL_KEY"],
-    :authentication       => "plain",
-    :enable_starttls_auto => true        
-  }
+
+config.action_mailer.default_url_options = { :host => 'http://young-island-7101.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "http://young-island-7101.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: zmb1391@gmail.com,
+  password: ENV["MAIL_KEY"]
+}
+
 
 
 end
