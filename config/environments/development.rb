ebbd1a301ed2fc939af0d0da631bd577dc1835ea
zac <zmb1391@gmail.com>
2014-05-15 18:05:45 -0400

    changed development.rb to have proper ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,9 @@ TeamManager::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['BUCKET_NAME'],
+      :access_key_id => ENV['AW_ID'],
+      :secret_access_key => ENV['AW_KEY']
     }
   }
 
