59241dd5b9fed5a54424fc88bdb87cac6b4098d2
zac <zmb1391@gmail.com>
2014-05-15 19:00:39 -0400

    test

--- app/models/home_page_file.rb
+++ app/models/home_page_file.rb
@@ -1,7 +1,7 @@
 class HomePageFile < ActiveRecord::Base
 
 	has_attached_file :the_file,
-					  :url => 's3-us-west-2.amazonaws.com'
+					  :s3_host_name => 's3-us-west-2.amazonaws.com'
 validates_attachment :the_file, :presence => true,
   :content_type => { :content_type => "application/pdf" },
   :on => :create
