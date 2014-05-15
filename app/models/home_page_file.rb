2c5e3a45673ed70895528b93611528af4f244dd9
zac <zmb1391@gmail.com>
2014-05-15 17:10:58 -0400

    set up home page to display schedule when attachments are present

--- app/models/home_page_file.rb
+++ app/models/home_page_file.rb
@@ -2,5 +2,6 @@ class HomePageFile < ActiveRecord::Base
 
 	has_attached_file :the_file
 validates_attachment :the_file, :presence => true,
-  :content_type => { :content_type => "application/pdf" }
+  :content_type => { :content_type => "application/pdf" },
+  :on => :create
 end
